4ced5ed89b0c95a041a77fb2b72e2022e006a699
SATaylor <engelstern7@yahoo.com>
2022-07-12 19:53:48 -0500

    working on the main layout

---
+++ components/NavBar.js
@@ -0,0 +1,109 @@
+import React, { useState } from 'react';
+import Link from 'next/link';
+
+const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <nav className='bg-slate-900 border-gray-200 px-4 lg:px-6 py-2.5'>
+      <div className='flex flex-wrap justify-between items-center mx-auto max-w-screen-xl'>
+        <button
+          onClick={handleClick}
+          data-collapse-toggle='mobile-menu-2'
+          type='button'
+          className='cursor-pointer md:hidden block p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 '
+          aria-controls='mobile-menu-2'
+          aria-expanded='false'>
+          <span className='sr-only'>Open main menu</span>
+          <svg
+            className='w-6 h-6'
+            fill='currentColor'
+            viewBox='0 0 20 20'
+            xmlns='http://www.w3.org/2000/svg'>
+            <path
+              fillRule='evenodd'
+              d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z'
+              clipRule='evenodd'></path>
+          </svg>
+          <svg
+            className='hidden w-6 h-6'
+            fill='currentColor'
+            viewBox='0 0 20 20'
+            xmlns='http://www.w3.org/2000/svg'>
+            <path
+              fillRule='evenodd'
+              d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z'
+              clipRule='evenodd'></path>
+          </svg>
+        </button>
+        <div
+          className={`${
+            isOpen ? '' : 'hidden'
+          } md:flex md:items-center md:w-auto w-full order-3 md:order-1`}
+          id='mobile-menu-2'>
+          <ul className='flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0'>
+            <li>
+              <a
+                href='#'
+                className='block py-2 pr-4 pl-3 text-white rounded bg-cyan-700 lg:bg-transparent lg:text-cyan-600 lg:p-0 '
+                aria-current='page'>
+                Shop
+              </a>
+            </li>
+            <li>
+              <a
+                href='#'
+                className='block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700'>
+                Custom Order
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className='order-1 md:order-2'>
+          <Link href='/'>
+            <a className='flex items-center p-2 mr-4'>
+              <svg
+                viewBox='0 0 24 24'
+                xmlns='http://www.w3.org/2000/svg'
+                className='fill-current text-white h-8 w-8 mr-2'>
+                <path d='M12.001 4.8c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624C13.666 10.618 15.027 12 18.001 12c3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C16.337 6.182 14.976 4.8 12.001 4.8zm-6 7.2c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624 1.177 1.194 2.538 2.576 5.512 2.576 3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C10.337 13.382 8.976 12 6.001 12z' />
+              </svg>
+              <span className='text-xl text-white font-bold uppercase tracking-wide'>
+                S.A.Taylor Customs
+              </span>
+            </a>
+          </Link>
+        </div>
+        <div className='order-2 md:order-3 flex items-center'>
+          <a
+            href='#'
+            className='text-gray-800 dark:text-white hover:text-cyan-500 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none'>
+            Log in
+          </a>
+
+          <a className='relative text-white hover:text-gray-200' href='#'>
+            <svg
+              className='h-5 w-5'
+              viewBox='0 0 24 24'
+              fill='none'
+              xmlns='http://www.w3.org/2000/svg'>
+              <path
+                d='M3 3H5L5.4 5M7 13H17L21 5H5.4M7 13L5.4 5M7 13L4.70711 15.2929C4.07714 15.9229 4.52331 17 5.41421 17H17M17 17C15.8954 17 15 17.8954 15 19C15 20.1046 15.8954 21 17 21C18.1046 21 19 20.1046 19 19C19 17.8954 18.1046 17 17 17ZM9 19C9 20.1046 8.10457 21 7 21C5.89543 21 5 20.1046 5 19C5 17.8954 5.89543 17 7 17C8.10457 17 9 17.8954 9 19Z'
+                stroke='currentColor'
+                strokeWidth='2'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+              />
+            </svg>
+
+            <span className='absolute top-0 left-0 rounded-full bg-cyan-500 text-white p-1 text-xs'></span>
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
